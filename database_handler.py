4c6fb3cdc413f635aa4c343d5dda6d5bd9478597
Pattarin <pattarin.won@ku.th>
2022-04-26 13:16:13 +0700

    raw structure

--- database_handler.py
+++ database_handler.py
@@ -1,19 +1,40 @@
 """A Class for connecting to database"""
+import psycopg2
+from dotenv import dotenv_values
+
+config = dotenv_values(".env")
 
 
 class DatabaseHandler:
 
     def __init__(self):
-        self.connect_to_db()
+        self.DATABASE_URL = config["DATABASE_URL"]
+        self.USER = config["PGUSER"]
+        self.PASSWORD = config["PGPASSWORD"]
+        self.PORT = config["PGPORT"]
+        self.DATABASE = config["PGDATABASE"]
+        self.HOST = config["PGHOST"]
+        self.db_cursor = None
 
-    async def connect_to_db(self):
+    async def db_cursor(self):
         """
         Connect to the database.
 
         Returns:
 
         """
-        return "successfully connect to the database"
+        # try:
+        return await psycopg2.connect(
+            dbname=self.DATABASE,
+            user=self.USER,
+            password=self.PASSWORD,
+            host=self.HOST,
+            port=self.PORT
+        ).cursor()
+        #     print("successfully connect to the database")
+        # except Exception as e:
+        #     SystemError("A problem happens during the db connection", e)
+        #     exit(1)
 
     def get_elephants_records(self):
         """
@@ -22,7 +43,11 @@ class DatabaseHandler:
         Returns:
             collections of elephant tracking records
         """
-        return
+        with self.db_cursor() as cs:
+            cs.execute(
+                """SELECT * FROM elephant_records"""
+            )
+        return cs.fetchall()
 
     def get_specific_record(self, record_number):
         """
@@ -35,3 +60,9 @@ class DatabaseHandler:
             a json record of requested record_number
         """
         return
+
+
+# if __name__ == '__main__':
+#     db = DatabaseHandler()
+#     result = await db.get_elephants_records()
+#     print(result)
--- info.py
+++ info.py
@@ -2,34 +2,33 @@ from fastapi import APIRouter
 from database_handler import DatabaseHandler
 
 router = APIRouter()
-db = DatabaseHandler()
 
-# @router.get("/users/", tags=["users"])
-# async def read_users():
-#     return [{"username": "Rick"}, {"username": "Morty"}]
+
+# async def db_connect():
+#     return await DatabaseHandler().db_cursor()
 #
 #
-# @router.get("/users/me", tags=["users"])
-# async def read_user_me():
-#     return {"username": "fakecurrentuser"}
+# db = db_connect()
+db = DatabaseHandler()
 
 
 @router.get("/info")
 async def get_elephants_records():
     """
-    Get the records of elephant from the database connection
-
+    Get the records of elephant from the database
     """
-    return db.get_elephants_records()
+    # return await db.get_elephants_records()
+    return "get a record"
 
 
 @router.get("/info/{record_number}")
-async def get_elephant_record(record_number):
+async def get_specific_record(record_number):
     """
     Get the specific record of elephant from db
 
     """
-    return db.get_specific_record(record_number)
+    # return await db.get_specific_record(record_number)
+    return "get a record"
 
 
 @router.get("/summary")
@@ -37,3 +36,15 @@ async def summary_elephants_records():
     """get the summary of elephant from the database"""
     return "return summary of elephant records form db"
 
+
+@router.post("/info")
+async def post_elephant_record():
+    """
+    Send a record of elephant to the database
+    """
+    return "send record"
+
+# if __name__ == '__main__':
+#     # print(db)
+#     result = get_elephants_records()
+#     print(result)

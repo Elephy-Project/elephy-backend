7a41ba5ef5b1acd796f351283b9750c11c27dd4b
Pattarin <pattarin.won@ku.th>
2022-06-07 15:40:30 +0700

    fix bug

--- main.py
+++ main.py
@@ -2,9 +2,14 @@ import uvicorn
 import info
 
 from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
 
 app = FastAPI()
 app.include_router(info.router)
+
+@app.get("/")
+def root():
+  return RedirectResponse("/docs")
 
 if __name__ == "__main__":
   uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)

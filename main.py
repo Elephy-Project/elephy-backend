0b4e072c85e51bed594a23e48faa680a22d3daf6
pattarinn <pattarin.wn@gmail.com>
2023-03-25 21:28:35 +0700

    edit CORS

--- main.py
+++ main.py
@@ -3,10 +3,24 @@ import info
 
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI()
 app.include_router(info.router)
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/", include_in_schema=False)
 def root():
